a0b8a307dd91a1276461a834eacc33bfdeab91f5
Punit1444 <punit@kgrp.in>
2024-01-18 20:44:39 +0530

    Add new and Edit functionality changes

---
+++ src/components/GST/firebase.js
@@ -0,0 +1,36 @@
+// firebase.js
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD33JyUFjugVISjRu_yElDamtvR0-6tBlU",
+    authDomain: "prototype-finkraft.firebaseapp.com",
+    projectId: "prototype-finkraft",
+    storageBucket: "prototype-finkraft.appspot.com",
+    messagingSenderId: "283994867558",
+    appId: "1:283994867558:web:d5038e6f45625e3c86eaa7",
+    measurementId: "G-6J2FHW78VS"
+}
+
+
+
+// export const auth = firebase.auth();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get individual Firebase services
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+// Get a list of cities from your database
+const credList = async function getCreds(firestore) {
+    const credsCol = collection(firestore, 'AirlinesCredentials');
+    const credsSnapshot = await getDocs(credsCol);
+    const credsList = credsSnapshot.docs.map(doc => doc.data());
+    console.log(credList);
+    return credsList;
+  }
+
+export { app, auth, firestore, credList, collection, getDocs };
---
+++ src/components/InProgressPage.js
@@ -0,0 +1,10 @@
+// InProgressPage.js
+import React from 'react';
+
+const InProgressPage = () => (
+  <div>
+    <h2>Page in Progress</h2>
+  </div>
+);
+
+export default InProgressPage;
